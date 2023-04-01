50f0dea2baecf0a705e69ee341f8f68cffd9c9b5
Christian <christian.nader@decathlon.com>
2023-04-01 11:14:49 +0200

    setup app entrypoint: 2 modes, cli (no logs) and reactive

--- src/main/java/chrisna/sandbox/mazes/MazesApplication.java
+++ src/main/java/chrisna/sandbox/mazes/MazesApplication.java
@@ -1,13 +1,27 @@
 package chrisna.sandbox.mazes;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.Banner;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class MazesApplication {
+public class MazesApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MazesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplicationBuilder appBuilder = new SpringApplicationBuilder(MazesApplication.class);
+        if (args.length > 0 && "cli".equals(args[0]))
+            appBuilder.web(WebApplicationType.NONE)
+                    .bannerMode(Banner.Mode.OFF)
+                    .logStartupInfo(false);
+        appBuilder.build().run(args);
+    }
 
+    @Override
+    public void run(String... args) {
+        if (args.length > 0 && "cli".equals(args[0])) {
+            System.out.println("TODO generate maze...");
+        }
+    }
 }
