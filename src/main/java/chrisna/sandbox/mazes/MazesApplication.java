35dcaaeb4b2dd326d8dfb64fedf46f42e835b32c
Christian <christian.nader@decathlon.com>
2023-04-01 10:26:58 +0200

    first commit

---
+++ src/main/java/chrisna/sandbox/mazes/MazesApplication.java
@@ -0,0 +1,13 @@
+package chrisna.sandbox.mazes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MazesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MazesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/chrisna/sandbox/mazes/MazesApplicationTests.java
@@ -0,0 +1,13 @@
+package chrisna.sandbox.mazes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MazesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
